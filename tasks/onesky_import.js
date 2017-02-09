e714df4a148541c148c8e3b60a5e48bee5c74ea2
VincentRich <vincent.richard@sportheroesgroup.com>
2017-02-09 11:53:55 +0100

    update on pull request

--- tasks/onesky_import.js
+++ tasks/onesky_import.js
@@ -10,6 +10,7 @@ var crypto = require('crypto');
 var fs = require('fs');
 var _ = require('lodash');
 var FormData = require('form-data');
+var Bluebird = require("bluebird");
 
 var apiRoot = 'https://platform.api.onesky.io/1/';
 
@@ -30,25 +31,25 @@ module.exports = function (grunt) {
             isKeepingAllStrings: true
         });
 
-        if (!!options.files.lenght) {
-            options.files.push({projectId: options.projectId, locale: options.locale, file: options.file});
+        if (!!options.files.length){
+            options.files.push({locale: options.locale, file: options.file});
         }
 
         uploadFiles = options.files.map(function(element) {
             return upload(element);
         });
 
-        Promise.all(uploadFiles).then(function() {
+        Bluebird.all(uploadFiles).then(function() {
             done();
         });
 
         ///////////////////////////
 
         function upload(element) {
-            var api = getApi(element.projectId);
+            var api = getApi();
             var url = api.baseUrl + api.path;
 
-            var _promise = new Promise(function(resolve, reject) {
+            return new Bluebird(function(resolve, reject) {
 
                 var form = new FormData();
                 form.append('api_key', api.publicKey);
@@ -96,15 +97,14 @@ module.exports = function (grunt) {
                     reject();
                 }
             });
-            return _promise;
         }
-        function getApi(projectId) {
+        function getApi() {
             var oneSkyKeys = grunt.file.readJSON(options.authFile);
             var timestamp = Math.floor(Date.now() / 1000);
             var devHash = crypto.createHash('md5').update(timestamp + oneSkyKeys.secretKey).digest('hex');
             return {
                 baseUrl: apiRoot,
-                path: 'projects/' + projectId + '/files',
+                path: 'projects/' + options.projectId + '/files',
                 publicKey: oneSkyKeys.publicKey,
                 timestamp: timestamp,
                 devHash: devHash
